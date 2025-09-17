ccebae45aad560270c87343305ad66ab34cb6fef
hassan-memon-09 <muhammadhassan.pitp@gmail.com>
2025-09-17 17:11:53 +0500

    Backend Deployement

---
+++ backend/src/app.controller.spec.ts
@@ -0,0 +1,21 @@
+// app.controller.spec.ts
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+
+describe('AppController', () => {
+  let appController: AppController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+    }).compile();
+
+    appController = app.get<AppController>(AppController);
+  });
+
+  describe('root', () => {
+    it('should return "NestJS backend is working!"', () => {
+      expect(appController.getRoot()).toBe('NestJS backend is working!');
+    });
+  });
+});
